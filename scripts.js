3bf1e49da6a99023497dc79bc7ec25d23ce5a636
mahesh4net <maheshkumarc890@gmail.com>
2024-07-28 00:44:43 +0530

    update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
+function togglemenu() {
+  const menuitem = document.getElementById("menudiv");
+  if (menuitem.style.opacity === "0") {
+    menuitem.style.opacity = "1";
+  } else {
+    menuitem.style.opacity = "0";
+  }
+}
 
+
+function toggleVisibility() {
+  const content = document.getElementById('menudiv');
+  content.classList.toggle('show');
+}
