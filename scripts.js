e7e5e7cedd86a3ecbe0c4227e8a86c6f73de0dfe
mahesh4net <maheshkumarc890@gmail.com>
2024-07-29 00:10:14 +0530

    update

--- scripts.js
+++ scripts.js
@@ -1,12 +1,3 @@
-function togglemenu() {
-  const menuitem = document.getElementById("menudiv");
-  if (menuitem.style.opacity === "0") {
-    menuitem.style.opacity = "1";
-  } else {
-    menuitem.style.opacity = "0";
-  }
-}
-
 
 function toggleVisibility() {
   const content = document.getElementById('menudiv');
